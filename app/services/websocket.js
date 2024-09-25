0739e8241f181d4ea8c06206f42f8f158c753963
ezhilselvan1109 <ezhilselvan1109@gmail.com>
2024-09-25 14:14:26 +0530

    enable browser notification

--- app/services/websocket.js
+++ app/services/websocket.js
@@ -2,33 +2,63 @@ import Service from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
 export default class WebsocketService extends Service {
-    socket = null;
-    @tracked data = [];
-    
-    connect(user_id) {
-        if (this.socket && this.socket.readyState === WebSocket.OPEN) return;
-        this.socket = new WebSocket(`ws://localhost:8080/facebook/notification?userId=${user_id}`);
-        this.socket.onopen = this.onOpen;
-        this.socket.onmessage = (event) => this.onMessage(event);
-        this.socket.onclose = this.onClose;
-    }
+  socket = null;
+  @tracked data = [];
 
-    close() {
-        if (this.socket && this.socket.readyState === WebSocket.OPEN) {
-            this.socket.close();
-        }
-    }
+  connect(user_id) {
+    if (this.socket && this.socket.readyState === WebSocket.OPEN) return;
+    this.socket = new WebSocket(`ws://localhost:8080/facebook/notification?userId=${user_id}`);
+    this.socket.onopen = this.onOpen;
+    this.socket.onmessage = (event) => this.onMessage(event);
+    this.socket.onclose = this.onClose;
+  }
 
-    onOpen() {
-        console.log('WebSocket connection opened');
+  close() {
+    if (this.socket && this.socket.readyState === WebSocket.OPEN) {
+      this.socket.close();
     }
+  }
 
-    onMessage(event) {
-        let arr = [JSON.parse(event.data)];
-        this.data = [...this.data, ...arr];
-    }
+  onOpen() {
+    console.log('WebSocket connection opened');
+  }
 
-    onClose() {
-        console.log('WebSocket connection closed');
+  async onMessage(event) {
+    let arr = [JSON.parse(event.data)];
+    this.data = [...this.data, ...arr];
+    const notification={
+        title:arr[0].user[0].first_name+" "+arr[0].user[0].last_name,
+        body:arr[0].comment
     }
+    let permission = Notification.permission;
+    if (permission === "granted") {
+        this.showNotification(notification);
+      } else if (permission === "default") {
+        await this.requestAndShowPermission(notification);
+      } else {
+        alert("Notification permission denied. Using normal alert instead.");
+      }
+  }
+
+  async requestAndShowPermission(notification) {
+    let permission = await Notification.requestPermission();
+    if (permission === "granted") {
+      this.showNotification(notification);
+    }
+  }
+
+  showNotification(notification) {
+    let title = notification.title;
+    let icon = '/assets/images/facebook-logo.png';
+    let body = notification.body;
+    try {
+      new Notification(title, { body, icon });
+    } catch (err) {
+      console.error('Notification error:', err);
+    }
+  }
+
+  onClose() {
+    console.log('WebSocket connection closed');
+  }
 }
